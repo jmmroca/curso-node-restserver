24ed00f76aa393a4cc5bb16da6cbe09ac2524bf5
Jose Manuel <jmmroca@uoc.edu>
2023-04-10 10:53:24 +0200

    .env borrado

--- database/config.js
+++ database/config.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-//const dotenv = require('dotenv');
 
 const dbConnection = async() => {
 
-    console.log(`La variable MONGODB_CNN vale ${process.env.MONGODB_CNN}`);
     try {
      
         await mongoose.connect(process.env.MONGODB_CNN, {
