6347083704eaa8a176284fabf2370bf21712e8bf
Jose Manuel <jmmroca@uoc.edu>
2023-04-10 10:40:52 +0200

    Visualizar variable BBDD

--- database/config.js
+++ database/config.js
@@ -6,14 +6,13 @@ const dbConnection = async() => {
     console.log(`La variable MONGODB_CNN vale ${process.env.MONGODB_CNN}`);
     try {
      
-    //     await mongoose.connect(process.env.MONGODB_CNN, {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true
-    //    });
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+       });
 
-        await mongoose.connect('mongodb+srv://user_node_cafe:mYafOnBkdTDh1Rod@cluster0.xuahyky.mongodb.net/cafeDB');
 
-        console.log('Base de datos online');
+       console.log('Base de datos online');
     } catch (error) {
         console.log(Error);
         throw new Error('Error a la hora de iniciar la base de datos');
