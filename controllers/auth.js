2e2c042dfc246af534922f022043d27b629a168a
Jose Manuel <jmmroca@uoc.edu>
2023-04-17 22:36:18 +0200

    Fin seccion 11 - Version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
 
 const Usuario = require('../models/usuario');
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
 
@@ -55,7 +56,54 @@ const login = async(req, res = response) => {
 
     
 }
+const googleSignIn = async(req, res = response ) => {
+
+        const {id_token} = req.body;
+
+        try {
+            
+            const { correo, nombre, img } = await googleVerify( id_token );
+
+            let usuario = await Usuario.findOne({ correo });
+
+            if (!usuario) {
+                const data = {
+                    nombre,
+                    correo,
+                    rol: 'USER_ROLE',
+                    password: ':P',
+                    img,
+                    google: true
+                };
+
+                usuario = new Usuario( data );
+                await usuario.save();
+            }
+            
+            if (!usuario.estado) {
+                return res.status(401).json({
+                    msg: 'Hable con el administrador, usuario bloqueado'
+                });
+            }
+
+            // Generar el JWT
+            const token = await generarJWT( usuario.id );
+
+
+            res.json({
+                usuario,
+                id_token
+            })
+        } catch (error) {
+            res.status(400).json({
+                ok: false,
+                msg:'El Token no se pudo verificar'
+            })
+        }
+
+}
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
